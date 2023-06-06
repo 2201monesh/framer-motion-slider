887489f85353794c92d670c6fb10d5a8ecc7c97e
Monesh Goyal <goyalmonesh2201@gmail.com>
2023-06-06 18:05:42 +0530

    file structure setup

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { useRef, useEffect, useState } from 'react';
+import {motion} from 'framer-motion';
+import './App.css';
+import images from './images';
+
+function App() {
+  return (
+    <div className="App">
+      <motion.div className='carousel'>
+        <motion.div className='inner-carousel'>
+          {images.map((image) => {
+            return (
+              <motion.div className='item'>
+                <img src={image} alt="" />
+              </motion.div>
+            )
+          })}
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/images.js
@@ -0,0 +1,7 @@
+import image1 from './assets/pexels-cottonbro-studio-5445015.jpg';
+import image2 from './assets/pexels-harrison-haines-3122801.jpg';
+import image3 from './assets/pexels-jarod-barton-13548705.jpg';
+import image4 from './assets/pexels-nur-andi-ravsanjani-gusma-3111410.jpg';
+import image5 from './assets/pexels-vedran-miletić-2341559.jpg';
+
+export default [image1, image2, image3, image4, image5];
